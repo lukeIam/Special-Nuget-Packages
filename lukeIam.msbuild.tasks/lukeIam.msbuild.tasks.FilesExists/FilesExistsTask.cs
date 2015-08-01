b387640f08671b9e2da0dbdd72e309819e99422f
lukeIam <mail2skywalker@gmail.com>
2015-08-01 20:43:56 +0200

    - Use compiled tasks for DownloadAndExtract package (faster and should work with xbuild)

---
+++ lukeIam.msbuild.tasks/lukeIam.msbuild.tasks.FilesExists/FilesExistsTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace lukeIam.nuget.tasks.FilesExists
+{
+    public class FilesExistsTask: Task
+    {
+        [Required]
+        public string Files { get; set; }
+
+        [Required]
+        public string Folder { get; set; }
+
+        [Output]
+        public bool Result { get; set; }
+
+        public override bool Execute()
+        {
+            if (String.IsNullOrEmpty(Files) || String.IsNullOrEmpty(Folder))
+            {
+                Log.LogError("Parameter must be not emty");
+                return false;
+            }
+
+            Result = true;
+            foreach (string file in Files.Split(';'))
+            {
+                try
+                {
+                    if (!File.Exists(Path.Combine(Folder, file)))
+                    {
+                        Result = false;
+                        break;
+                    }
+                }
+                catch (ArgumentException e)
+                {
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ lukeIam.msbuild.tasks/lukeIam.msbuild.tasks.ZipDownload/ZipDownloadTask.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using Ionic.Zip;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+namespace lukeIam.msbuild.tasks.ZipDownload
+{
+    public class ZipDownloadTask : Task
+    {
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string TargetFolder { get; set; }
+
+        public override bool Execute()
+        {
+            if (String.IsNullOrEmpty(Address) || String.IsNullOrEmpty(TargetFolder))
+            {
+                Log.LogError("Parameter must be not emty");
+                return false;
+            }
+
+            if (!Directory.Exists(TargetFolder))
+            {
+                Directory.CreateDirectory(TargetFolder);
+            }
+
+            using (WebClient wc = new WebClient())
+            {
+                byte[] downloadedData = null;
+
+                try
+                {
+                    downloadedData = wc.DownloadData(Address);
+                }
+                catch (WebException e)
+                {
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
+                catch (NotSupportedException e)
+                {
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
+
+                try
+                {
+                    using (ZipInputStream zip = new ZipInputStream(new MemoryStream(downloadedData)))
+                    {
+                        ZipEntry zipEntry = zip.GetNextEntry();
+                        while (zipEntry != null)
+                        {
+                            var name = zipEntry.FileName;
+                            if (zipEntry.IsDirectory)
+                            {
+                                Directory.CreateDirectory(Path.Combine(TargetFolder, name));
+                            }
+                            else
+                            {
+                                var path = Path.Combine(TargetFolder, name);
+                                var folder = Path.GetDirectoryName(path);
+                                if (!String.IsNullOrEmpty(folder) && ! Directory.Exists(folder))
+                                {
+                                    Directory.CreateDirectory(folder);
+                                }
+
+                                FileStream writer = File.Create(path);
+                                
+                                byte[] data = new byte[4096];
+                                int bytesRead = 1;
+                                while (bytesRead > 0)
+                                {
+                                    bytesRead = zip.Read(data, 0, data.Length);
+                                    writer.Write(data, 0, bytesRead);
+                                }
+                                writer.Close();
+                            }
+
+                            zipEntry = zip.GetNextEntry();
+                        }
+                    }
+                }
+                catch(BadReadException e)
+                {
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
+                catch (BadStateException e)
+                {
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
+                catch (ZipException e)
+                {
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
+                catch (Ionic.Zlib.ZlibException e)
+                {
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
